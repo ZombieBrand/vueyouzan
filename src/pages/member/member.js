4670a5e6a5b254eeaf837c7ddc03ec31cb0b50bd
Your Name <you@example.com>
2019-03-05 20:58:11 +0800

    3.5

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,2 +1,10 @@
 import './member_base.css'
 import './member.css'
+// 第一步使用vue-router
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+// 创建vue-router实例
+let router = new Router({
+    
+})
