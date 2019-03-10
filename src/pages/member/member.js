162eadd6ea32c7a54c3f7835922f75126661b28c
Your Name <you@example.com>
2019-03-10 22:46:01 +0800

    3.10

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,10 +1,34 @@
-import './member_base.css'
-import './member.css'
 // 第一步使用vue-router
 import Vue from 'vue'
-import Router from 'vue-router'
-Vue.use(Router)
+import VueRouter from 'vue-router'
+import member from './components/Member.vue'
+import address from './components/Address.vue'
+import all from './components/All.vue'
+import form from './components/Form.vue'
+Vue.use(VueRouter)
 // 创建vue-router实例
-let router = new Router({
-    
+const routes = [{
+  path: '/',
+  component: member
+}, {
+  path: '/address',
+  component: address,
+  children: [{
+    path:'',
+    component:all
+  },{
+    path: 'all',
+    component: all
+  }, {
+    path: '/form',
+    component:form
+  }]
+}]
+const router = new VueRouter({
+  routes
+})
+// 根组件注入
+new Vue({
+  el: '#app',
+  router
 })
